41e469c8edb49ed377e9230de893b785f02deea9
Noah Gordon <tortuga.ng@gmail.com>
2014-08-29 23:31:57 -0400

    Mid-css edit commit.

--- app/assets/javascripts/views/root.js
+++ app/assets/javascripts/views/root.js
@@ -1,6 +1,10 @@
 AirNZG.Views.Root = Backbone.View.extend({	
   template: JST['root'],
 	
+	tagName: "article",
+	
+	className: "root-content",
+	
 	render: function() {
 		var content = this.template();
 		this.$el.html(content);
