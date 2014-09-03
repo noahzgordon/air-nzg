1115b950e9d24415ad38034c0935adf4eb1f84eb
Noah Gordon <tortuga.ng@gmail.com>
2014-09-03 09:41:04 -0400

    Initialize backbone app with a current user property.

--- app/assets/javascripts/air_nzg.js
+++ app/assets/javascripts/air_nzg.js
@@ -3,8 +3,14 @@ window.AirNZG = {
   Collections: {},
   Views: {},
   Routers: {},
-  initialize: function() {
-		AirNZG.users.fetch();
+  initialize: function(options) {
+		// sets a top-level current user property 
+		AirNZG.users.fetch({
+			success: function(collection) {
+				AirNZG.currentUser = collection.getOrFetch(options.currentUserId)
+			}
+		});
+		
     new AirNZG.Routers.App();
 		Backbone.history.start({ root: "/" });
   }
