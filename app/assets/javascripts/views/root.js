bd66f0f5a8999c75d35ab359bf63447b50208f4e
Noah Gordon <tortuga.ng@gmail.com>
2014-09-03 09:31:10 -0400

    Add loading gif to home url so that it appears during long intializations.

--- app/assets/javascripts/views/root.js
+++ app/assets/javascripts/views/root.js
@@ -5,6 +5,17 @@ AirNZG.Views.Root = Backbone.View.extend({
 	
 	className: "root-content",
 	
+	events: {
+		"submit .listing-search": "executeQuery"
+	},
+	
+	executeQuery: function(event) {
+		event.preventDefault();
+		var queryString = $(event.currentTarget).serialize()
+
+		Backbone.history.navigate("#/listings?" + queryString, { trigger: true })	
+	},
+	
 	render: function() {
 		var content = this.template();
 		this.$el.html(content);
