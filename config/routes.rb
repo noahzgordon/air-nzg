536f80d2c6fc024ec08ea3757a973de380b30c40
Noah Gordon <tortuga.ng@gmail.com>
2014-09-03 12:10:29 -0400

    Begin the great gutting. Remove all obsolete Rails controllers. Remove header from applications.rb and comments from home static page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  resource :session, only: [:new, :create, :destroy]
-  resources :users, except: [:index, :destroy]
-  
   namespace :api, defaults: { format: :json } do
     resource :session, only: [:create, :destroy]
     
@@ -13,14 +10,6 @@ Rails.application.routes.draw do
     get '/my_listings', to: 'listings#my_listings'
     get '/my_bookings', to: 'bookings#my_bookings'
   end
-
-  resources :listings
-  get 'my_listings', to: 'listings#my_listings'
-  
-  resources :bookings, only: [:create]
-  get '/my_booking_requests', to: 'bookings#my_booking_requests'
-  patch '/bookings/approve/:id', to: 'bookings#approve', as: 'approve_booking'
-  patch '/bookings/deny/:id', to: 'bookings#deny', as: 'deny_booking'
   
   get 'auth/facebook/callback', to: 'oauth_callbacks#facebook'
   
